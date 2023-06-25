95d096bf95707a1fdcc582de8c6c77d143d50d4e
aditstywn <137696058+aditstywn@users.noreply.github.com>
2023-06-25 19:52:08 +0700

    Delete web_rpl directory

--- web_rpl/user/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#keyword").on("keyup", function(){
-        $.get("../ajax/produk.php?keyword=" + $("#keyword").val(),function(data){
-            $("#container").html(data);
-        });
-    });
-});
